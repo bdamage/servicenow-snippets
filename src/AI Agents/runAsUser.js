24d52192a8781ba53dbb8f79ff116e7ec9ceb3a5
bdamage <kjell.lloyd@gmail.com>
2025-09-09 08:19:25 +0200

    structured some files

---
+++ src/AI Agents/runAsUser.js
@@ -0,0 +1,10 @@
+/**
+ * Script to be evaluated at runtime when the trigger is executed.
+ * @param {GlideRecord} current - Target record that executed this trigger.
+ * @returns {string}
+ */
+(function (current) {
+    var result = "6816f79cc0a8016401c5a33be04be441"; // sys_id of the user to run the triggered agent or agentic workflow
+    // write your code here...
+    return result;
+})(current);
